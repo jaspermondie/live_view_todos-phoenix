f08d7d86ed573932a04a5698fbd1dd167b7138ec
jaspermondie <jasper.mondares@gmail.com>
2019-05-18 13:05:02 +1000

    Displays the Todo list

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -1,11 +1,16 @@
 defmodule LiveViewTodosWeb.TodoLive do
     use Phoenix.LiveView
+
+    alias LiveViewTodos.Todos
+    alias LiveViewTodosWeb.TodoView
   
     def mount(_session, socket) do
-      {:ok, socket}
+      # {:ok, socket}
+      {:ok, assign(socket, todos: Todos.list_todos())}
     end
   
     def render(assigns) do
-      ~L"Rendering LiveView"
+      # ~L"Rendering LiveView"
+      TodoView.render("todos.html", assigns)
     end
 end
