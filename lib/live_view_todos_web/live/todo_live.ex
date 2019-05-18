8afe946450d041d5c219c88b6c33c6dbe3918307
jaspermondie <jasper.mondares@gmail.com>
2019-05-19 03:43:48 +1000

    Adds update of todo

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -18,6 +18,16 @@ defmodule LiveViewTodosWeb.TodoLive do
       {:noreply, fetch(socket)}
     end
 
+    def handle_event("update", %{"todo" => new_todo}, socket) do
+      old_todo = Todos.get_todo!(Map.get(new_todo, "id"))
+      Todos.update_todo(old_todo, new_todo)
+      {:noreply, fetch(socket)}
+    end
+
+    def handle_event("edit_todo", val, socket) do
+      {:noreply, update(socket, :edit_id, fn _ -> String.to_integer(val) end)}
+    end
+
     def handle_event("toggle_done", id, socket) do
       todo = Todos.get_todo!(id)
       Todos.update_todo(todo, %{done: !todo.done})
@@ -30,12 +40,16 @@ defmodule LiveViewTodosWeb.TodoLive do
       {:noreply, socket}
     end
 
+    def handle_event("cancel_edit", _, socket) do
+      {:noreply, update(socket, :edit_id, fn _ -> -1 end)}
+    end
+
     def handle_info({Todos, [:todo | _], _}, socket) do
       {:noreply, fetch(socket)}
     end
 
     defp fetch(socket) do
-      assign(socket, todos: Todos.list_todos())
+      assign(socket, todos: Todos.list_todos(), edit_id: -1)
     end
 
 end
